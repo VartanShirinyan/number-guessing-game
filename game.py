19e1b6151768d4dd3b44e75ac44cb21eb9f3a203
VartanShirinyan <02vartan-s@mail.ru>
2025-11-21 14:44:21 +0300

    feat: implemented counting of the number of attempts. (реализован подсчет количества попыток)

--- game.py
+++ game.py
@@ -9,13 +9,16 @@ print("Удачи!")
 print("(Заглушка: игра еще в разработке)")
 
 secret_number = random.randint(1, 100)
-print(secret_number)
+count = 0
+
 while True:
-    user_number = int(input('Введите Ваше число!'))
+    user_number = int(input('Введите Ваше число: '))
     if user_number > secret_number:
         print('Ваше число больше загаданного, попробуйте еще разок')
+        count += 1
     elif user_number < secret_number:
         print('Ваше число меньше загаданного, попробуйте еще разок')
+        count += 1
     else:
-        print('Вы угадали, поздравляем!')
+        print(f'Поздравляю, Вы Угадали за {count} попыток(ки)!')
         break
